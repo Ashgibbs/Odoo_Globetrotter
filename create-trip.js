c35463d70801e637baf36f22a4d8c1f603553c96
Vignesh-015 <vignesh007890@gmail.com>
2026-01-03 15:11:31 +0530

    plan trip

---
+++ create-trip.js
@@ -0,0 +1,8 @@
+function goBack() {
+  window.location.href = "dashboard.html";
+}
+
+function continueToItinerary() {
+  // DB save will be added in next step
+  window.location.href = "itinerary-builder.html";
+}
